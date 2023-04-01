229c909d7b60623422819af1fc18905add7467ef
Siarhei Melnik <siarhei.m.a@gmail.com>
2023-04-01 10:48:51 +0300

    feat: add smoothing to atr

--- ta/atr_indicator.py
+++ ta/atr_indicator.py
@@ -4,7 +4,7 @@ class ATRIndicator:
     def __init__(self, period=14):
         self.period = period
 
-    def atr(self, data):
+    def atr(self, data, smoothing='rma'):
         data = data.copy()
         data['previous_close'] = data['close'].shift(1)
 
@@ -14,5 +14,16 @@ class ATRIndicator:
         true_range['low_previous_close'] = abs(data['low'] - data['previous_close'])
 
         data['true_range'] = true_range.max(axis=1)
-        data['atr'] = data['true_range'].rolling(window=self.period).mean()
+        
+        if smoothing == 'rma':
+            data['atr'] = data['true_range'].ewm(span=self.period, adjust=False).mean()
+        elif smoothing == 'wilder':
+            data['atr'] = 0.0
+            
+            data.at[self.period, 'atr'] = data['true_range'][1:self.period + 1].mean()
+            for i in range(self.period + 1, len(data)):
+                data.at[i, 'atr'] = (data.at[i - 1, 'atr'] * (self.period - 1) + data.at[i, 'true_range']) / self.period
+        else:
+            data['atr'] = data['true_range'].rolling(window=self.period).mean()
+        
         return data['atr']
