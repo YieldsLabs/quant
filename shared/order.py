c50cf9d3ba51b77d8c53431485bab5e4f5d99460
Siarhei Melnik <siarhei.m.a@gmail.com>
2023-04-03 22:35:52 +0300

    refactor: convert order to dataclass

--- shared/order.py
+++ shared/order.py
@@ -1,16 +1,18 @@
+from dataclasses import dataclass
+
 from shared.order_side import OrderSide
 
 
+@dataclass
 class Order:
-    def __init__(self, timestamp: int, side: OrderSide, entry_price: float, exit_price: float, stop_loss: float, take_profit: float, pnl: float):
-        self.id = None
-        self.timestamp = timestamp
-        self.side = side
-        self.entry_price = entry_price
-        self.exit_price = exit_price
-        self.stop_loss = stop_loss
-        self.take_profit = take_profit
-        self.pnl = pnl
+    timestamp: int
+    side: OrderSide
+    entry_price: float
+    exit_price: float
+    stop_loss: float
+    take_profit: float
+    pnl: float
+    id: str = None
 
     def to_dict(self):
         return {
