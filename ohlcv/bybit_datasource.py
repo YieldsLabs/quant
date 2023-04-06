b35ad3bdc83c9a7914f7b61d1b0dfcbf70957c85
Siarhei Melnik <siarhei.m.a@gmail.com>
2023-04-06 19:39:06 +0300

    feat: improve strategy screening optimization

--- ohlcv/bybit_datasource.py
+++ ohlcv/bybit_datasource.py
@@ -1,14 +1,33 @@
-
+import time
 from typing import Type
 from broker.abstract_broker import AbstractBroker
 from ohlcv.abstract_datasource import AbstractDatasource
 from shared.timeframes import Timeframes
-
+from requests.exceptions import RequestException
 
 class BybitDataSource(AbstractDatasource):
-    def __init__(self, broker: Type[AbstractBroker], lookback=1000):
+    def __init__(self, broker: Type[AbstractBroker], lookback=1000, max_retries=7, initial_retry_delay=3):
         self.broker = broker
         self.lookback = lookback
+        self.max_retries = max_retries
+        self.initial_retry_delay = initial_retry_delay
 
     def fetch(self, symbol: str, timeframe: Timeframes):
-        return self.broker.get_historical_data(symbol, timeframe.value, lookback=self.lookback)
+        retries = 0
+
+        while retries < self.max_retries:
+            try:
+                return self.broker.get_historical_data(symbol, timeframe.value, lookback=self.lookback)
+
+            except RequestException as e:
+                print(f"Request error: {e}. Retrying...")
+
+            except Exception as e:
+                print(f"Unexpected error: {e}. Retrying...")
+
+            retry_delay = self.initial_retry_delay * (2 ** retries)
+            print(f"Waiting {retry_delay} seconds before retrying.")
+            time.sleep(retry_delay)
+            retries += 1
+
+        raise Exception(f"Failed to fetch data for {symbol} after {self.max_retries} retries.")
---
+++ optimization/strategy_screening.py
@@ -0,0 +1,60 @@
+from concurrent.futures import ThreadPoolExecutor
+from itertools import product
+from typing import List, Type
+
+import pandas as pd
+from analytics.abstract_performace import AbstractPerformance
+from broker.abstract_broker import AbstractBroker
+from ohlcv.context import OhlcvContext
+from risk_management.stop_loss.base.abstract_stop_loss_finder import AbstractStopLoss
+from risk_management.take_profit.abstract_take_profit_finder import AbstractTakeProfit
+from strategy.abstract_strategy import AbstractStrategy
+from trader.backtester import Backtester
+from optimization.abstract_screening import AbstractScreening
+from risk_management.risk_manager import RiskManager
+
+
+class StrategyScreening(AbstractScreening):
+    def __init__(self, ohlcv: Type[OhlcvContext], broker: Type[AbstractBroker], analytics: Type[AbstractPerformance], symbols: List[str], timeframes: List[str], strategies: List[AbstractStrategy], stop_loss_finders: List[AbstractStopLoss], take_profit_finders: List[AbstractTakeProfit], num_last_trades=15):
+        super().__init__(ohlcv)
+        self.broker = broker
+        self.analytics = analytics
+        self.symbols = symbols
+        self.timeframes = timeframes
+        self.strategies = strategies
+        self.stop_loss_finders = stop_loss_finders
+        self.take_profit_finders = take_profit_finders
+        self.num_last_trades = num_last_trades
+
+    def _run_backtest(self, settings, rm):
+        symbol, timeframe, strategy = settings
+
+        backtester = Backtester(self.ohlcv_context, self.broker, rm, self.analytics)
+
+        result = backtester.trade(strategy, symbol, timeframe)
+
+        result.update({
+            'symbol': symbol,
+            'timeframe': timeframe,
+            'strategy': strategy,
+            'stop_loss_finder': rm.stop_loss_finder,
+            'take_profit_finder': rm.take_profit_finder,
+            'win_rate': result['win_rate'] * 100,
+        })
+
+        print(f"{symbol} orders for strategy: {strategy}, TimeFrame: {timeframe}, StopLossFinder: {rm.stop_loss_finder}, TakeProfitFinder: {rm.take_profit_finder}")
+        print(backtester.get_orders().tail(self.num_last_trades))
+        return result
+
+    def run(self):
+        market_dict = {symbol: self.broker.get_symbol_info(symbol) for symbol in self.symbols}
+        
+        risk_manager_dict = {(symbol, stop_loss_finder, take_profit_finder): RiskManager(stop_loss_finder, take_profit_finder, **market_dict[symbol]) for symbol in self.symbols for stop_loss_finder, take_profit_finder in product(self.stop_loss_finders, self.take_profit_finders)}
+
+        combined_settings = list(product(self.symbols, self.timeframes, self.strategies, risk_manager_dict.keys()))
+
+        with ThreadPoolExecutor() as executor:
+            results_list = [executor.submit(self._run_backtest, settings[:3], risk_manager_dict[settings[0], settings[3][1], settings[3][2]]) for settings in combined_settings]
+            results_list = [result.result() for result in results_list]
+
+        return pd.DataFrame(results_list).sort_values(by="total_pnl", ascending=False)
