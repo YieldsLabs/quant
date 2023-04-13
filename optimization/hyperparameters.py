6361c82d882c197fc4ff3934f6484e186301da6e
Siarhei Melnik <siarhei.m.a@gmail.com>
2023-04-13 09:20:50 +0300

    feat: change hyperparameters space

--- optimization/hyperparameters.py
+++ optimization/hyperparameters.py
@@ -7,10 +7,12 @@ strategy_hyperparameters = {
     'overbought': [10, 30, 5],
     'oversold': [70, 90, 5],
     'sma_period': [20, 100, 10],
-    'slow_sma_period': [50, 200, 2],
+    'slow_sma_period': [50, 200, 50],
     'stdev_multi': [0.5, 2, 0.5],
     'tolerance': [0.002, 0.01, 0.002],
-    'lookback': [50, 150, 50]
+    'lookback': [50, 150, 50],
+    'ao_short': [3, 15, 2.5],
+    'ao_long': [20, 50, 15]
 }
 
 stoploss_hyperparameters = {
@@ -18,7 +20,7 @@ stoploss_hyperparameters = {
 }
 
 takeprofit_hyperparameters = {
-    'risk_reward_ratio': [1, 5.5, 0.5],
+    'risk_reward_ratio': [1, 6, 0.5],
 }
 
 
