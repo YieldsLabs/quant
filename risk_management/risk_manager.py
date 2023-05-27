ca6cd1dacbf2c6f76998fbd351a246b869e09c07
Siarhei Melnik <siarhei.m.a@gmail.com>
2023-05-27 15:58:48 +0300

    did stateless rm

--- risk_management/risk_manager.py
+++ risk_management/risk_manager.py
@@ -1,6 +1,5 @@
-import asyncio
-from enum import Enum, auto
 from core.event_dispatcher import register_handler
+from core.events.ohlcv import OHLCV
 from core.events.position import PositionSide
 from core.events.risk import RiskEvaluate, RiskExit, RiskType
 from risk_management.abstract_risk_manager import AbstractRiskManager
@@ -13,40 +12,39 @@ class RiskManager(AbstractRiskManager):
         super().__init__()
         self.risk_type = risk_type
         self.break_even_strategy = BreakEvenStopLossStrategy()
-        self.stop_loss_lock = asyncio.Lock()
 
     @register_handler(RiskEvaluate)
     async def _on_risk(self, event: RiskEvaluate):
         symbol, timeframe, position_side, position_size, entry_price, stop_loss_price, risk_reward_ratio, risk_per_trade, ohlcv, strategy = self._unpack_event(event)
+        take_profit_price = self._calculate_take_profit_price(risk_reward_ratio, entry_price, stop_loss_price)
 
-        take_profit_price = None
+        if self.risk_type == RiskType.BREAK_EVEN and stop_loss_price is not None:
+            stop_loss_price = await self._calculate_break_even_stop_loss(symbol, position_side, position_size, stop_loss_price, entry_price, ohlcv, risk_per_trade)
 
-        async with self.stop_loss_lock:
-            if stop_loss_price:
-                take_profit_price = RiskRewardTakeProfitFinder(risk_reward_ratio).next(entry_price, stop_loss_price)
+        if self._should_exit(position_side, stop_loss_price, take_profit_price, ohlcv):
+            await self._process_exit(symbol, timeframe, position_side, strategy, ohlcv, take_profit_price, stop_loss_price)
 
-                if self.risk_type == RiskType.BREAK_EVEN:
-                    stop_loss_price = self.break_even_strategy.next(symbol, position_side, position_size, stop_loss_price, entry_price, ohlcv.low, ohlcv.high, risk_per_trade)
+    async def _calculate_break_even_stop_loss(self, symbol, position_side, position_size, stop_loss_price, entry_price, ohlcv, risk_per_trade):
+        return await self.break_even_strategy.next(symbol, position_side, position_size, stop_loss_price, entry_price, ohlcv.low, ohlcv.high, risk_per_trade)
 
-        if not self._should_exit(position_side, stop_loss_price, take_profit_price, ohlcv.low, ohlcv.high):
-            return
+    def _calculate_take_profit_price(self, risk_reward_ratio, entry_price, stop_loss_price):
+        return RiskRewardTakeProfitFinder(risk_reward_ratio).next(entry_price, stop_loss_price)
 
-        async with self.stop_loss_lock:
-            self.break_even_strategy.reset(symbol, position_side)
+    async def _process_exit(self, symbol, timeframe, position_side, strategy, ohlcv, take_profit_price, stop_loss_price):
+        await self.break_even_strategy.reset(symbol, position_side)
 
         exit_price = self._calculate_exit_price(position_side, ohlcv.close, take_profit_price, stop_loss_price)
 
-        await self.dispatcher.dispatch(
-            RiskExit(symbol=symbol, timeframe=timeframe, side=position_side, strategy=strategy, exit=exit_price))
+        await self.dispatcher.dispatch(RiskExit(symbol=symbol, timeframe=timeframe, side=position_side, strategy=strategy, exit=exit_price))
 
     def _unpack_event(self, event: RiskEvaluate):
         return event.symbol, event.timeframe, event.side, event.size, event.entry, event.stop_loss, event.risk_reward_ratio, event.risk_per_trade, event.ohlcv, event.strategy
 
-    def _should_exit(self, position_side: PositionSide, stop_loss_price: float | None, take_profit_price: float | None, low: float, high: float):
+    def _should_exit(self, position_side: PositionSide, stop_loss_price: float | None, take_profit_price: float | None, ohlcv: OHLCV):
         if position_side == PositionSide.LONG:
-            return self._long_exit_conditions(stop_loss_price, take_profit_price, low, high)
+            return self._long_exit_conditions(stop_loss_price, take_profit_price, ohlcv.low, ohlcv.high)
         elif position_side == PositionSide.SHORT:
-            return self._short_exit_conditions(stop_loss_price, take_profit_price, low, high)
+            return self._short_exit_conditions(stop_loss_price, take_profit_price, ohlcv.low, ohlcv.high)
 
     @staticmethod
     def _calculate_exit_price(position_side: PositionSide, current_close: float, take_profit_price: float | None, stop_loss_price: float | None):
@@ -61,11 +59,11 @@ class RiskManager(AbstractRiskManager):
         return exit_price
 
     @staticmethod
-    def _long_exit_conditions(stop_loss_price: float | None, take_profit_price: float | None, low: float , high: float):
+    def _long_exit_conditions(stop_loss_price: float | None, take_profit_price: float | None, low: float, high: float):
         return (stop_loss_price is not None and low <= stop_loss_price) or \
                (take_profit_price is not None and high >= take_profit_price)
 
     @staticmethod
-    def _short_exit_conditions(stop_loss_price: float | None, take_profit_price: float | None, low: float , high: float):
+    def _short_exit_conditions(stop_loss_price: float | None, take_profit_price: float | None, low: float, high: float):
         return (stop_loss_price is not None and high >= stop_loss_price) or \
                (take_profit_price is not None and low <= take_profit_price)
--- risk_management/stop_loss/strategy/break_even.py
+++ risk_management/stop_loss/strategy/break_even.py
@@ -1,39 +1,47 @@
+import asyncio
 from core.events.position import PositionSide
+from core.timeframe import Timeframe
 
 
 class BreakEvenStopLossStrategy:
     def __init__(self):
         self.data = {}
+        self.lock = asyncio.Lock()
 
-    def _init(self, symbol: str):
-        self.data[symbol] = {PositionSide.LONG: None, PositionSide.SHORT: None}
+    async def _init(self, loss_id: str):
+        async with self.lock:
+            self.data[loss_id] = {PositionSide.LONG: None, PositionSide.SHORT: None}
 
-    def next(self, symbol: str, position_side: PositionSide, position_size: float, stop_loss_price: float, entry_price: float, low: float, high: float, risk_per_trade: float) -> float:
-        if symbol not in self.data:
-            self._init(symbol)
+    async def next(self, symbol: str, timeframe: Timeframe, position_side: PositionSide, position_size: float, stop_loss_price: float, entry_price: float, low: float, high: float, risk_per_trade: float) -> float:
+        loss_id = self._create_loss_id(symbol, timeframe)
 
-        if not self.data[symbol][position_side]:
-            self.data[symbol][position_side] = stop_loss_price
+        if loss_id not in self.data:
+            await self._init(loss_id)
 
-        if position_side == PositionSide.LONG:
-            new_stop_loss_price = high - (high - stop_loss_price) * risk_per_trade
+        if not self.data[loss_id][position_side]:
+            self.data[loss_id][position_side] = stop_loss_price
 
-            if new_stop_loss_price > self.data[symbol][position_side]:
-                self.data[symbol][position_side] = new_stop_loss_price
-                if self.data[symbol][position_side] - entry_price >= risk_per_trade * position_size:
-                    self.data[symbol][position_side] = entry_price
+        self.data[loss_id][position_side] = self._calculate_stop_loss(loss_id, position_side, position_size, stop_loss_price, entry_price, low, high, risk_per_trade)
 
-        elif position_side == PositionSide.SHORT:
-            new_stop_loss_price = low + (stop_loss_price - low) * risk_per_trade
+        return self.data[loss_id][position_side]
 
-            if new_stop_loss_price < self.data[symbol][position_side]:
-                self.data[symbol][position_side] = new_stop_loss_price
-                if entry_price - self.data[symbol][position_side] >= risk_per_trade * position_size:
-                    self.data[symbol][position_side] = entry_price
+    def _calculate_stop_loss(self, loss_id, position_side, position_size, stop_loss_price, entry_price, low, high, risk_per_trade):
+        new_stop_loss_price = high - (high - stop_loss_price) * risk_per_trade if position_side == PositionSide.LONG else low + (stop_loss_price - low) * risk_per_trade
 
-        next_stop_loss_price = self.data[symbol][position_side]
+        if ((position_side == PositionSide.LONG and new_stop_loss_price > self.data[loss_id][position_side])
+                or (position_side == PositionSide.SHORT and new_stop_loss_price < self.data[loss_id][position_side])):
+            self.data[loss_id][position_side] = new_stop_loss_price
 
-        return next_stop_loss_price
+        if abs(self.data[loss_id][position_side] - entry_price) >= risk_per_trade * position_size:
+            self.data[loss_id][position_side] = entry_price
 
-    def reset(self, symbol: str, position_side: PositionSide):
-        self.data[symbol][position_side] = None
+        return self.data[loss_id][position_side]
+
+    async def reset(self, symbol: str, timeframe: Timeframe, position_side: PositionSide):
+        async with self.lock:
+            loss_id = self._create_loss_id(symbol, timeframe)
+            self.data[loss_id][position_side] = None
+
+    @staticmethod
+    def _create_loss_id(symbol: str, timeframe: Timeframe) -> str:
+        return f'{symbol}_{timeframe}'
