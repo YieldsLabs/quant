567c71cc4b1d592e27aade5bb18eacef3f533f87
m5l14i11 <siarhei.melnik@proton.me>
2023-07-13 23:44:17 +0300

    refactor

--- ta_lib/core/src/bool.rs
+++ ta_lib/core/src/bool.rs
@@ -1,27 +1,34 @@
 use crate::series::Series;
 
 impl Series<f64> {
+    fn compare<F>(&self, scalar: f64, f: F) -> Series<bool>
+    where
+        F: Fn(f64, f64) -> bool,
+    {
+        self.fmap(|x| x.map(|v| f(*v, scalar)))
+    }
+
     pub fn eq(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v == scalar))
+        self.compare(scalar, |a, b| a == b)
     }
 
     pub fn ne(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v != scalar))
+        self.compare(scalar, |a, b| a != b)
     }
 
     pub fn gt(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v > scalar))
+        self.compare(scalar, |a, b| a > b)
     }
 
     pub fn gte(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v >= scalar))
+        self.compare(scalar, |a, b| a >= b)
     }
 
     pub fn lt(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v < scalar))
+        self.compare(scalar, |a, b| a < b)
     }
 
     pub fn lte(&self, scalar: f64) -> Series<bool> {
-        self.fmap(|x| x.map(|v| *v <= scalar))
+        self.compare(scalar, |a, b| a <= b)
     }
 }
