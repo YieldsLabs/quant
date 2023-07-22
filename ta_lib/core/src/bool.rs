fdfc6ebb2086e8a9cf922e6d2b13be0c134611e0
m5l14i11 <siarhei.melnik@proton.me>
2023-07-22 16:56:53 +0300

    series bool

--- ta_lib/core/src/bool.rs
+++ ta_lib/core/src/bool.rs
@@ -1,6 +1,16 @@
 use crate::series::Series;
 
 impl Series<f64> {
+    fn compare_series<F>(&self, rhs: &Series<f64>, f: F) -> Series<bool>
+    where
+        F: Fn(f64, f64) -> bool,
+    {
+        self.zip_with(rhs, |a, b| match (a, b) {
+            (Some(a_val), Some(b_val)) => Some(f(*a_val, *b_val)),
+            _ => None,
+        })
+    }
+
     fn compare<F>(&self, scalar: f64, f: F) -> Series<bool>
     where
         F: Fn(f64, f64) -> bool,
@@ -30,5 +40,29 @@ impl Series<f64> {
 
     pub fn lte(&self, scalar: f64) -> Series<bool> {
         self.compare(scalar, |a, b| a <= b)
+    }
+
+    pub fn eq_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a == b)
+    }
+
+    pub fn ne_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a != b)
+    }
+
+    pub fn gt_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a > b)
+    }
+
+    pub fn gte_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a >= b)
+    }
+
+    pub fn lt_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a < b)
+    }
+
+    pub fn lte_series(&self, rhs: &Series<f64>) -> Series<bool> {
+        self.compare_series(rhs, |a, b| a <= b)
     }
 }
