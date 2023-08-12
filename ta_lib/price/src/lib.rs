82cf952cdd3568ec2741916ab4c1e3773d1362bb
m5l14i11 <siarhei.melnik@proton.me>
2023-08-12 14:37:06 +0300

    fix

--- ta_lib/price/src/lib.rs
+++ ta_lib/price/src/lib.rs
@@ -3,7 +3,7 @@ mod median;
 mod typical;
 mod wcl;
 
-pub use average::{average_price};
-pub use median::{median_price};
-pub use typical::{typical_price};
-pub use wcl::{wcl};
+pub use average::average_price;
+pub use median::median_price;
+pub use typical::typical_price;
+pub use wcl::wcl;
