d4c211ac47af0f339ca723cb4badf2f115ca7033
m5l14i11 <siarhei.melnik@proton.me>
2023-08-31 23:06:32 +0300

    wip

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -13,21 +13,60 @@ def create_candlestick(data: pd.DataFrame) -> go.Figure:
     )
 
 def create_candlestick(data: pd.DataFrame) -> go.Figure:
-    return go.Figure(go.Candlestick(
+    return go.Candlestick(
         x=data['timestamp'],
         open=data['open'],
         high=data['high'],
         low=data['low'],
-        close=data['close']
-    ))
+        close=data['close'],
+        increasing_line_color='green',
+        decreasing_line_color='red' ,
+        name="OHLC"
+    )
 
-def plot_candlestick(title: str, data: pd.DataFrame) -> go.Figure:
-    fig = create_candlestick(data)
+def plot_candlestick(data: pd.DataFrame) -> go.Figure:
+    n = len(data)
+    rows = (n + 1) // 3
+    cols = 3
+    
+    fig = make_subplots(
+        rows=rows,
+        cols=cols,
+        shared_xaxes=False,
+        subplot_titles=list(data.keys()),
+        vertical_spacing=0.1,
+    )
+
+    row = 1
+    col = 1
+    
+    for title, df in data.items():
+        candlestick = create_candlestick(df)
+        
+        fig.add_trace(candlestick, row=row, col=col)
+        
+        if col == 3:
+            col = 1
+            row += 1
+        else:
+            col += 1
 
     fig.update_layout(
-        title=title,
-        xaxis_rangeslider_visible=False,
-        height=600,
-        width=1200
+        showlegend=False,
+        template='plotly_dark',
+        height=320 * rows,
+        width=1100,
+        legend_orientation="h",
+        plot_bgcolor='black',
+        paper_bgcolor='black',
+        margin=dict(
+            autoexpand=False,
+            l=40,
+            r=15,
+            t=60,
+            b=40
+        )
     )
+
+    fig.update_xaxes(rangeslider_visible=False)
     return fig
