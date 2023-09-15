25acaa54bf2de718b492a145935a53d5cc7616ce
m5l14i11 <siarhei.melnik@proton.me>
2023-09-15 21:55:19 +0300

    wip

--- ta_lib/strategies/trend_follow/src/ffi.rs
+++ ta_lib/strategies/trend_follow/src/ffi.rs
@@ -2,15 +2,8 @@ use crate::CrossMAStrategy;
 use crate::TestingGroundStrategy;
 use base::register_strategy;
 
-#[no_mangle]
-pub fn register_crossma(
-    smothing: usize,
-    short_period: usize,
-    long_period: usize,
-    atr_period: usize,
-    stop_loss_multi: f32,
-) -> i32 {
-    let ma = match smothing {
+fn map_to_ma(smothing: usize) -> &'static str {
+    match smothing {
         1 => "ALMA",
         2 => "DEMA",
         3 => "EMA",
@@ -29,8 +22,18 @@ pub fn register_crossma(
         16 => "WMA",
         17 => "ZLEMA",
         _ => "SMA",
-    };
+    }
+}
 
+#[no_mangle]
+pub fn register_crossma(
+    smothing: usize,
+    short_period: usize,
+    long_period: usize,
+    atr_period: usize,
+    stop_loss_multi: f32,
+) -> i32 {
+    let ma = map_to_ma(smothing);
     let strategy = CrossMAStrategy::new(ma, short_period, long_period, atr_period, stop_loss_multi);
     register_strategy(Box::new(strategy))
 }
@@ -42,27 +45,7 @@ pub fn register_ground(
     atr_period: usize,
     stop_loss_multi: f32,
 ) -> i32 {
-    let ma = match smothing {
-        1 => "ALMA",
-        2 => "DEMA",
-        3 => "EMA",
-        4 => "FRAMA",
-        5 => "GMA",
-        6 => "HMA",
-        7 => "KAMA",
-        8 => "RMSMA",
-        9 => "SINWMA",
-        10 => "SMA",
-        11 => "SMMA",
-        12 => "T3",
-        13 => "TEMA",
-        14 => "TMA",
-        15 => "VWMA",
-        16 => "WMA",
-        17 => "ZLEMA",
-        _ => "SMA",
-    };
-
+    let ma = map_to_ma(smothing);
     let strategy = TestingGroundStrategy::new(ma, long_period, atr_period, stop_loss_multi);
     register_strategy(Box::new(strategy))
 }
