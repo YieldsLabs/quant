c1b6ba2b476364d23a5889e3ccc9efb81423ddcd
m5l14i11 <siarhei.melnik@proton.me>
2023-09-18 14:47:39 +0300

    wip

--- ta_lib/indicators/momentum/src/lib.rs
+++ ta_lib/indicators/momentum/src/lib.rs
@@ -1,5 +1,13 @@
-pub mod aosc;
-pub mod macd;
-pub mod rsi;
-pub mod sso;
-pub mod stochosc;
+mod aosc;
+mod macd;
+mod roc;
+mod rsi;
+mod sso;
+mod stochosc;
+
+pub use aosc::aosc;
+pub use macd::macd;
+pub use roc::roc;
+pub use rsi::rsi;
+pub use sso::sso;
+pub use stochosc::stochosc;
