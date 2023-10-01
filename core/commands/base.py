2762371b4bd512dc827b38499736562d6c20dae3
m5l14i11 <siarhei.melnik@proton.me>
2023-10-01 13:15:39 +0300

    idempotency

--- core/commands/base.py
+++ core/commands/base.py
@@ -1,5 +1,6 @@
 import asyncio
-from dataclasses import dataclass, field
+import hashlib
+from dataclasses import dataclass, field, fields
 
 from core.events.base import Event, EventMeta
 
@@ -14,3 +15,14 @@ class Command(Event):
 
     async def wait_for_execution(self):
         await self._execution_event.wait()
+
+    def __post_init__(self):
+        attribute_values = [
+            getattr(self, field.name)
+            for field in fields(self)
+            if field.name not in ["meta", "_execution_event"]
+        ]
+        concatenated = f"{self.__class__.__name__}{attribute_values}"
+        idempotency_key = hashlib.sha256(concatenated.encode("utf-8")).hexdigest()
+
+        object.__setattr__(self, "meta", EventMeta(priority=1, key=idempotency_key))
