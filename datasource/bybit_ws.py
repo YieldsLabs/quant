ad64ff6daacddc12e8ed104452e8993108b976d6
m5l14i11 <siarhei.melnik@proton.me>
2023-11-07 16:05:02 +0300

    upd

--- datasource/bybit_ws.py
+++ datasource/bybit_ws.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 import websockets
+from websockets.exceptions import ConnectionClosedError
 
 from core.commands.broker import Subscribe
 from core.event_decorators import command_handler
@@ -25,35 +26,42 @@ class BybitWSHandler(AbstractWS):
         Timeframe.FOUR_HOURS: 240,
     }
 
-    TIMEFRAMES = {
-        "1": Timeframe.ONE_MINUTE,
-        "3": Timeframe.THREE_MINUTES,
-        "5": Timeframe.FIVE_MINUTES,
-        "15": Timeframe.FIFTEEN_MINUTES,
-        "60": Timeframe.ONE_HOUR,
-        "240": Timeframe.FOUR_HOURS,
-    }
+    TIMEFRAMES = {str(v): k for k, v in INTERVALS.items()}
+
+    SUBSCRIBE_OPERATION = "subscribe"
+    KLINE_CHANNEL = "kline"
+    TOPIC_KEY = "topic"
+    DATA_KEY = "data"
+    CONFIRM_KEY = "confirm"
 
     def __init__(self, url):
         super().__init__()
         self.url = url
         self.ws = None
 
-    @retry(max_retries=5, initial_retry_delay=5, handled_exceptions=(Exception,))
+    @retry(
+        max_retries=5,
+        initial_retry_delay=5,
+        handled_exceptions=(ConnectionError, ConnectionClosedError),
+    )
     async def connect_to_websocket(self):
         self.ws = await websockets.connect(self.url)
 
     async def process_message(self, message):
         message_data = json.loads(message)
 
-        if "topic" in message_data:
-            ohlcv = message_data["data"][0]
-            topic = message_data["topic"].split(".")
+        if self.TOPIC_KEY in message_data:
+            ohlcv = message_data[self.DATA_KEY][0]
+            topic = message_data[self.TOPIC_KEY].split(".")
             symbol, interval = topic[2], topic[1]
+
+            if interval not in self.TIMEFRAMES:
+                logger.error(f"Unknown interval: {interval}")
+                return None
 
             ohlcv_event = self.parse_candle_message(symbol, interval, ohlcv)
 
-            if ohlcv["confirm"]:
+            if ohlcv[self.CONFIRM_KEY]:
                 logger.info(f"Tick: {symbol}:{interval}:{ohlcv_event}")
 
             await self.dispatch(ohlcv_event)
@@ -61,49 +69,42 @@ class BybitWSHandler(AbstractWS):
     async def send_ping(self, interval):
         while True:
             await asyncio.sleep(interval)
-            try:
-                if self.ws.open:
-                    await self.ws.ping()
-                else:
-                    raise websockets.exceptions.ConnectionClosedOK(
-                        None,
-                        websockets.frames.Close(code=1000, reason="Connection closed"),
-                    )
-            except websockets.exceptions.ConnectionClosedOK:
-                logger.info("Connection closed.")
-                await self.connect_to_websocket()
-            except websockets.exceptions.ConnectionClosedError:
-                logger.info("Connection closed with error")
-                await self.connect_to_websocket()
-            except Exception as e:
-                logger.error(f"Error while keep alive ping: {e}")
+
+            if not self.ws:
+                continue
+
+            if self.ws.open:
+                await self.ws.ping()
+            else:
+                logger.info("WebSocket not open, attempting to reconnect...")
                 await self.connect_to_websocket()
 
     async def process_messages(self):
         while True:
-            try:
-                async for message in self.ws:
-                    await self.process_message(message)
-            except websockets.exceptions.ConnectionClosedOK:
-                logger.info("Connection closed.")
-                await self.connect_to_websocket()
-            except websockets.exceptions.ConnectionClosedError:
-                logger.info("Connection closed with error")
-                await self.connect_to_websocket()
-            except Exception as e:
-                logger.error(f"Error while process message: {e}")
-                await self.connect_to_websocket()
+            message = await self.ws.recv()
 
-    async def run(self, ping_interval=20):
+            if message is None:
+                break
+
+            await self.process_message(message)
+
+    async def run(self, ping_interval=10):
         try:
             await self.connect_to_websocket()
 
             ping_task = asyncio.create_task(self.send_ping(interval=ping_interval))
             message_processing_task = asyncio.create_task(self.process_messages())
+            
+            done, pending = await asyncio.wait(
+                [ping_task, message_processing_task], return_when=asyncio.FIRST_EXCEPTION
+            )
 
-            await asyncio.gather(ping_task, message_processing_task)
-        except ConnectionError as e:
-            logger.error(f"Could not establish WebSocket connection: {e}")
+            for task in pending:
+                task.cancel()
+
+            for task in done:
+                if task.exception():
+                    logger.error(f"Task raised an exception: {task.exception()}")
         finally:
             if self.ws:
                 await self.ws.close()
@@ -111,18 +112,20 @@ class BybitWSHandler(AbstractWS):
     def parse_candle_message(self, symbol, interval, data):
         ohlcv = OHLCV.from_dict(data)
         return NewMarketDataReceived(
-            symbol, self.TIMEFRAMES[interval], ohlcv, data["confirm"]
+            symbol, self.TIMEFRAMES[interval], ohlcv, data[self.CONFIRM_KEY]
         )
 
     @command_handler(Subscribe)
     async def subscribe(self, command: Subscribe):
-        if self.ws is None:
-            raise ValueError("Initialize ws")
+        if not self.ws or not self.ws.open:
+            raise ValueError("WebSocket is not connected or open.")
 
         channels = [
-            f"kline.{self.INTERVALS[timeframe]}.{symbol}"
-            for (symbol, timeframe) in command.symbols_and_timeframes
+            f"{self.KLINE_CHANNEL}.{self.INTERVALS[timeframe]}.{symbol}"
+            for symbol, timeframe in command.symbols_and_timeframes
         ]
 
-        for channel in channels:
-            await self.ws.send(json.dumps({"op": "subscribe", "args": [channel]}))
+        subscribe_message = json.dumps(
+            {"op": self.SUBSCRIBE_OPERATION, "args": channels}
+        )
+        await self.ws.send(subscribe_message)
