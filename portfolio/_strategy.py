3eb94fda32ca56653407b55f4529cf7e2030398a
m5l14i11 <siarhei.melnik@proton.me>
2023-12-15 20:03:34 +0300

    upd

--- portfolio/_strategy.py
+++ portfolio/_strategy.py
@@ -26,33 +26,19 @@ class StrategyStorage:
     ):
         async with self.lock:
             key = (symbol, timeframe, strategy)
-
             self.data[key] = (metrics, -1)
 
-            if len(self.data.keys()) < self.kmeans.n_clusters:
-                return
-
-            data_matrix = np.array([item[0] for item in self.data.values()])
-
-            normalized_data = self.scaler.fit_transform(data_matrix)
-            cluster_indices = self.kmeans.fit_predict(normalized_data)
-
-            for idx, key in enumerate(self.data.keys()):
-                current_metrics = self.data[key][0]
-                self.data[key] = (current_metrics, cluster_indices[idx])
+            if len(self.data.keys()) >= self.kmeans.n_clusters:
+                self._update_clusters()
 
     async def reset(self, symbol: Symbol, timeframe: Timeframe, strategy: Strategy):
         async with self.lock:
-            key = (symbol, timeframe, strategy)
-            if key in self.data:
-                del self.data[key]
+            self.data.pop((symbol, timeframe, strategy), None)
 
     async def get_top(self, num: int = 10):
         async with self.lock:
             sorted_strategies = sorted(
-                self.data.keys(),
-                key=lambda key: (self.data[key][1], self.data[key][0][0]),
-                reverse=True,
+                self.data.keys(), key=self._sorting_key, reverse=True
             )
 
             selected_symbols = set()
@@ -67,3 +53,19 @@ class StrategyStorage:
                         break
 
             return top_strategies
+
+    def _update_clusters(self):
+        data_matrix = np.array([item[0] for item in self.data.values()])
+        normalized_data = self.scaler.fit_transform(data_matrix)
+        cluster_indices = self.kmeans.fit_predict(normalized_data)
+
+        for (symbol, timeframe, strategy), idx in zip(
+            self.data.keys(), cluster_indices
+        ):
+            self.data[(symbol, timeframe, strategy)] = (
+                self.data[(symbol, timeframe, strategy)][0],
+                idx,
+            )
+
+    def _sorting_key(self, key):
+        return self.data[key][1], self.data[key][0][0]
