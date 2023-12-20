c416d5109d7537fec5ce377edfe134bb89f73617
m5l14i11 <siarhei.melnik@proton.me>
2023-12-20 19:03:50 +0300

    upd

--- ta_lib/benches/indicators.rs
+++ ta_lib/benches/indicators.rs
@@ -1,9 +1,38 @@
 use core::prelude::*;
 use criterion::{criterion_group, criterion_main, Criterion};
-use momentum::{apo, stc};
+use momentum::{ao, apo, stc};
+use price::prelude::*;
 
 fn momentum(c: &mut Criterion) {
     let mut group = c.benchmark_group("momentum");
+
+    group.bench_function("ao", |b| {
+        b.iter_batched_ref(
+            || {
+                let high = Series::from([
+                    6.8430, 6.8660, 6.8685, 6.8690, 6.865, 6.8595, 6.8565, 6.862, 6.859, 6.86,
+                    6.8580, 6.8605, 6.8620, 6.86, 6.859, 6.8670, 6.8640, 6.8575, 6.8485, 6.8450,
+                    7.1195, 7.136, 7.1405, 7.112, 7.1095, 7.1220, 7.1310, 7.1550, 7.1480, 7.1435,
+                    7.1405, 7.1440, 7.1495, 7.1515, 7.1415, 7.1445, 7.1525, 7.1440, 7.1370, 7.1305,
+                    7.1375, 7.1250, 7.1190, 7.1135, 7.1280, 7.1220, 7.1230, 7.1225, 7.1180, 7.1250,
+                ]);
+                let low = Series::from([
+                    6.8380, 6.8430, 6.8595, 6.8640, 6.8435, 6.8445, 6.8510, 6.8560, 6.8520, 6.8530,
+                    6.8550, 6.8550, 6.8565, 6.8475, 6.8480, 6.8535, 6.8565, 6.8455, 6.8445, 6.8365,
+                    7.1195, 7.136, 7.1405, 7.112, 7.1095, 7.1220, 7.1310, 7.1550, 7.1480, 7.1435,
+                    7.1405, 7.1440, 7.1495, 7.1515, 7.1415, 7.1445, 7.1525, 7.1440, 7.1370, 7.1305,
+                    7.1375, 7.1250, 7.1190, 7.1135, 7.1280, 7.1220, 7.1230, 7.1225, 7.1180, 7.1250,
+                ]);
+                let source = median_price(&high, &low);
+                let short_period = 5;
+                let long_period = 34;
+
+                (source, short_period, long_period)
+            },
+            |(source, short_period, long_period)| ao(source, *short_period, *long_period),
+            criterion::BatchSize::SmallInput,
+        )
+    });
 
     group.bench_function("apo", |b| {
         b.iter_batched_ref(
