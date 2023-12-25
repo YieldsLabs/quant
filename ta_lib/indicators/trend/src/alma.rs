c739481f0b162ab9532a197dba66dc791eac7cb3
m5l14i11 <siarhei.melnik@proton.me>
2023-12-25 15:00:23 +0300

    upd

--- ta_lib/indicators/trend/src/alma.rs
+++ ta_lib/indicators/trend/src/alma.rs
@@ -3,15 +3,15 @@ use core::prelude::*;
 pub fn alma(source: &Series<f32>, period: usize, offset: f32, sigma: f32) -> Series<f32> {
     let m = offset * (period as f32 - 1.);
     let s = period as f32 / sigma;
-
     let mut sum = Series::zero(source.len());
-    let mut norm = 0.;
 
-    for i in 0..period {
-        let weight = ((-1. * (i as f32 - m).powi(2)) / (2. * s.powi(2))).exp();
+    let weights = (0..period)
+        .map(|i| ((-1. * (i as f32 - m).powi(2)) / (2. * s.powi(2))).exp())
+        .collect::<Vec<_>>();
+    let norm = weights.iter().sum::<f32>();
 
-        norm += weight;
-        sum = sum + source.shift(period - i - 1) * weight;
+    for (i, &weight) in weights.iter().enumerate() {
+        sum = sum + source.shift(i) * weight;
     }
 
     sum / norm
@@ -23,8 +23,19 @@ mod tests {
 
     #[test]
     fn test_alma() {
-        let source = Series::from([1.0, 2.0, 3.0, 4.0, 5.0]);
-        let expected = [0.0, 0.0, 2.6856735, 3.6856735, 4.6856737];
+        let source = Series::from([
+            0.01707, 0.01706, 0.01707, 0.01705, 0.01710, 0.01705, 0.01704, 0.01709,
+        ]);
+        let expected = [
+            0.0,
+            0.0,
+            0.017066907,
+            0.01705621,
+            0.017084463,
+            0.017065462,
+            0.017043246,
+            0.017074436,
+        ];
         let epsilon = 0.001;
 
         let result: Vec<f32> = alma(&source, 3, 0.85, 6.0).into();
