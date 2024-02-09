cfdc87bd7cc67c73b64bee65db3f08872e03a36c
m5l14i11 <siarhei.melnik@proton.me>
2024-02-09 13:32:40 +0300

    upd

--- ta_lib/indicators/trend/src/tma.rs
+++ ta_lib/indicators/trend/src/tma.rs
@@ -1,9 +1,10 @@
 use core::prelude::*;
 
 pub fn tma(source: &Series<f32>, period: usize) -> Series<f32> {
-    let n = (period as f32 / 2. + 1.) as usize;
+    let n = (period as f32 / 2.).signum() as usize;
+    let m = (period as f32 / 2. + 1.) as usize;
 
-    source.smooth(Smooth::SMA, n).smooth(Smooth::SMA, n)
+    source.smooth(Smooth::SMA, n).smooth(Smooth::SMA, m)
 }
 
 #[cfg(test)]
@@ -13,7 +14,7 @@ mod tests {
     #[test]
     fn test_tma() {
         let source = Series::from([1.0, 2.0, 3.0, 4.0, 5.0]);
-        let expected = vec![1.0, 1.25, 2.0, 3.0, 4.0];
+        let expected = vec![1.0, 1.5, 2.5, 3.5, 4.5];
 
         let result: Vec<f32> = tma(&source, 3).into();
 
