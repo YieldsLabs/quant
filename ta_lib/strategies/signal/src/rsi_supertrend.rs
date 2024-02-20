f3c09c71a364bbdf6e55004674234b535f356e33
m5l14i11 <siarhei.melnik@proton.me>
2024-02-20 17:59:52 +0300

    upd

--- ta_lib/strategies/signal/src/rsi_supertrend.rs
+++ ta_lib/strategies/signal/src/rsi_supertrend.rs
@@ -53,12 +53,22 @@ impl Signal for RSISupertrendSignal {
 
         (
             direction.seq(&1.0)
+                & (direction.shift(1).seq(&-1.0)
+                    | direction.shift(2).seq(&-1.0)
+                    | direction.shift(3).seq(&-1.0)
+                    | direction.shift(4).seq(&-1.0)
+                    | direction.shift(5).seq(&-1.0))
                 & rsi.sgt(&RSI_NEUTRALITY)
                 & rsi.slt(&upper_barrier)
                 & rsi.shift(1).sgt(&lower_neutrality)
                 & rsi.shift(2).sgt(&lower_neutrality)
                 & rsi.shift(3).sgt(&lower_neutrality),
             direction.seq(&-1.0)
+                & (direction.shift(1).seq(&1.0)
+                    | direction.shift(2).seq(&1.0)
+                    | direction.shift(3).seq(&1.0)
+                    | direction.shift(4).seq(&1.0)
+                    | direction.shift(5).seq(&1.0))
                 & rsi.slt(&RSI_NEUTRALITY)
                 & rsi.sgt(&lower_barrier)
                 & rsi.shift(1).slt(&upper_neutrality)
