b0846a2664c6c0f2600db0fcff332825f42a9545
m5l14i11 <siarhei.melnik@proton.me>
2024-04-05 13:58:40 +0300

    upd

--- core/models/moving_average.py
+++ core/models/moving_average.py
@@ -2,33 +2,33 @@ from enum import Enum
 
 
 class MovingAverageType(Enum):
-    # ALMA = 1
+    ALMA = 1
     CAMA = 2
-    # DEMA = 3
-    # EMA = 4
-    # FRAMA = 5
-    # GMA = 6
-    # HMA = 7
-    # HEMA = 8
-    # KAMA = 9
-    # KJS = 10
-    # LSMA = 11
-    # MD = 12
-    # RMSMA = 13
-    # SINWMA = 14
-    # SMA = 15
-    # SMMA = 16
-    # T3 = 17
-    # TEMA = 18
-    # TMA = 19
-    # VIDYA = 20
-    # VWMA = 21
-    # VWEMA = 22
-    # WMA = 23
-    # ZLEMA = 24
-    # ZLSMA = 25
-    # ZLTEMA = 26
-    # ZLHMA = 27
+    DEMA = 3
+    EMA = 4
+    FRAMA = 5
+    GMA = 6
+    HMA = 7
+    HEMA = 8
+    KAMA = 9
+    KJS = 10
+    LSMA = 11
+    MD = 12
+    RMSMA = 13
+    SINWMA = 14
+    SMA = 15
+    SMMA = 16
+    T3 = 17
+    TEMA = 18
+    TMA = 19
+    VIDYA = 20
+    VWMA = 21
+    VWEMA = 22
+    WMA = 23
+    ZLEMA = 24
+    ZLSMA = 25
+    ZLTEMA = 26
+    ZLHMA = 27
 
     def __str__(self):
         return self.name.upper()
