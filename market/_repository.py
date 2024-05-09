d27d1856b8af92bfe90aeb2f343a60edc4df78c7
m5l14i11 <siarhei.melnik@proton.me>
2024-05-09 21:18:40 +0300

    upd

--- market/_repository.py
+++ market/_repository.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from core.interfaces.abstract_market_repository import AbstractMarketRepository
+from core.interfaces.abstract_wasm_service import AbstractWasmService
 from core.models.ohlcv import OHLCV
 from core.models.symbol import Symbol
 from core.models.timeframe import Timeframe
@@ -8,26 +9,27 @@ from core.models.timeseries import Timeseries
 
 
 class MarketRepository(AbstractMarketRepository):
-    def __init__(self):
+    def __init__(self, wasm_service: AbstractWasmService):
         super().__init__()
         self._lock = asyncio.Lock()
-        self._store = {}
+        self._bucket = {}
+        self.wasm_service = wasm_service
 
     async def upsert(self, symbol: Symbol, timeframe: Timeframe, bar: OHLCV):
-        store = await self._get_store(symbol, timeframe)
-        store.enqueue(bar)
+        timeseries = await self._get_timeseries(symbol, timeframe)
+        timeseries.add(bar)
 
     async def find_next_bar(self, symbol: Symbol, timeframe: Timeframe, bar: OHLCV):
-        store = await self._get_store(symbol, timeframe)
+        timeseries = await self._get_timeseries(symbol, timeframe)
 
-        async for next_bar in store.find_next_bar(bar):
+        async for next_bar in timeseries.find_next_bar(bar):
             yield next_bar
 
-    async def _get_store(self, symbol: Symbol, timeframe: Timeframe):
+    async def _get_timeseries(self, symbol: Symbol, timeframe: Timeframe) -> Timeseries:
         async with self._lock:
             key = (symbol, timeframe)
 
-            if key not in self._store:
-                self._store[key] = Timeseries()
+            if key not in self._bucket:
+                self._bucket[key] = Timeseries()
 
-            return self._store[key]
+            return self._bucket[key]
