3e2a8d321c43147c2b65bf9ddaafcb9e91dc5e00
m5l14i11 <siarhei.melnik@proton.me>
2024-06-02 14:48:19 +0300

    upd

--- strategy/generator/baseline/ma.py
+++ strategy/generator/baseline/ma.py
@@ -4,6 +4,7 @@ from core.models.moving_average import MovingAverageType
 from core.models.parameter import (
     CategoricalParameter,
     Parameter,
+    RandomParameter,
     StaticParameter,
 )
 from core.models.source import SourceType
@@ -16,4 +17,4 @@ class MaBaseLine(BaseLine):
     type: BaseLineType = BaseLineType.Ma
     source_type: Parameter = StaticParameter(SourceType.CLOSE)
     ma: Parameter = CategoricalParameter(MovingAverageType)
-    period: Parameter = StaticParameter(14.0)
+    period: Parameter = RandomParameter(6.0, 14.0)
