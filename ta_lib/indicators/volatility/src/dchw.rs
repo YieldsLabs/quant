8f39a945a965cbb5106532e39497c9eea02ca4ef
m5l14i11 <siarhei.melnik@proton.me>
2024-06-05 20:47:23 +0300

    upd

---
+++ ta_lib/indicators/volatility/src/dchw.rs
@@ -0,0 +1,26 @@
+use crate::dch;
+use core::prelude::*;
+
+pub fn dchw(high: &Series<f32>, low: &Series<f32>, period: usize) -> Series<f32> {
+    let (upb, _, lb) = dch(high, low, period);
+
+    upb - lb
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dchw() {
+        let high = Series::from([1.0, 2.0, 3.0, 4.0, 5.0]);
+        let low = Series::from([1.0, 2.0, 3.0, 4.0, 5.0]);
+        let period = 3;
+
+        let expected = vec![0.0, 1.0, 2.0, 2.0, 2.0];
+
+        let dchw: Vec<f32> = dchw(&high, &low, period).into();
+
+        assert_eq!(dchw, expected);
+    }
+}
