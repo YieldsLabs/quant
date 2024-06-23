d7e5020e5354202404f1dab191d59060b8a14712
m5l14i11 <siarhei.melnik@proton.me>
2024-06-23 18:18:27 +0300

    upd

--- ta_lib/core/src/distance.rs
+++ ta_lib/core/src/distance.rs
@@ -11,7 +11,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ad() {
+    fn test_average_distance() {
         let source = Series::from([1.0, 2.0, 3.0, 4.0, 5.0]);
         let expected = Series::from([0.0, 0.5, 1.0, 1.0, 1.0]);
         let period = 3;
