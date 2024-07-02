f0f21854fd4d90426270cc15986a598cc5c51fa4
m5l14i11 <siarhei.melnik@proton.me>
2024-07-02 22:30:47 +0300

    upd

--- ta_lib/strategies/pulse/src/wae.rs
+++ ta_lib/strategies/pulse/src/wae.rs
@@ -4,31 +4,31 @@ use timeseries::prelude::*;
 use volatility::bb;
 
 pub struct WaePulse {
-    smooth_type: Smooth,
-    fast_period: usize,
-    slow_period: usize,
+    smooth: Smooth,
+    period_fast: usize,
+    period_slow: usize,
     smooth_bb: Smooth,
-    bb_period: usize,
+    period_bb: usize,
     factor: f32,
     strength: f32,
 }
 
 impl WaePulse {
     pub fn new(
-        smooth_type: Smooth,
-        fast_period: f32,
-        slow_period: f32,
+        smooth: Smooth,
+        period_fast: f32,
+        period_slow: f32,
         smooth_bb: Smooth,
-        bb_period: f32,
+        period_bb: f32,
         factor: f32,
         strength: f32,
     ) -> Self {
         Self {
-            smooth_type,
-            fast_period: fast_period as usize,
-            slow_period: slow_period as usize,
+            smooth,
+            period_fast: period_fast as usize,
+            period_slow: period_slow as usize,
             smooth_bb,
-            bb_period: bb_period as usize,
+            period_bb: period_bb as usize,
             factor,
             strength,
         }
@@ -37,21 +37,24 @@ impl WaePulse {
 
 impl Pulse for WaePulse {
     fn lookback(&self) -> usize {
-        let adj_lookback = std::cmp::max(self.fast_period, self.slow_period);
-        std::cmp::max(adj_lookback, self.bb_period)
+        let adj_lookback = std::cmp::max(self.period_fast, self.period_slow);
+        std::cmp::max(adj_lookback, self.period_bb)
     }
 
     fn assess(&self, data: &OHLCVSeries) -> (Series<bool>, Series<bool>) {
-        let (upper_bb, _, lower_bb) = bb(data.close(), self.smooth_bb, self.bb_period, self.factor);
+        let (upper_bb, _, lower_bb) = bb(data.close(), self.smooth_bb, self.period_bb, self.factor);
+
         let e = upper_bb - lower_bb;
 
-        let prev_close = data.close().shift(1);
+        let spread = data
+            .close()
+            .spread(self.smooth, self.period_fast, self.period_slow);
+        let prev_spread =
+            data.close()
+                .shift(1)
+                .spread(self.smooth, self.period_fast, self.period_slow);
 
-        let macd_line = data.close().smooth(self.smooth_type, self.fast_period)
-            - data.close().smooth(self.smooth_type, self.slow_period);
-        let prev_macd_line = prev_close.smooth(self.smooth_type, self.fast_period)
-            - prev_close.smooth(self.smooth_type, self.slow_period);
-        let t = (macd_line - prev_macd_line) * self.strength;
+        let t = (spread - prev_spread) * self.strength;
 
         let zero = Series::zero(data.len());
 
