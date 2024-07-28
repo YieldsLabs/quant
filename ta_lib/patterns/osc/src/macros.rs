7f62af3bba6f309a552b4fa5c0964a17f39ce535
m5l14i11 <siarhei.melnik@proton.me>
2024-07-29 01:36:49 +0300

    upd

--- ta_lib/patterns/osc/src/macros.rs
+++ ta_lib/patterns/osc/src/macros.rs
@@ -1,4 +1,28 @@
 #[macro_export]
+macro_rules! a {
+    ($osc:expr, $lower_const:expr, $upper_const:expr) => {{
+        let prev_osc = $osc.shift(1);
+
+        (
+            $osc.slt(&$lower_const) & prev_osc.sgt(&$lower_const),
+            $osc.sgt(&$upper_const) & prev_osc.slt(&$upper_const),
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! c {
+    ($osc:expr, $lower_const:expr, $upper_const:expr) => {{
+        let prev_osc = $osc.shift(1);
+
+        (
+            $osc.sgt(&$lower_const) & prev_osc.slt(&$lower_const),
+            $osc.slt(&$upper_const) & prev_osc.sgt(&$upper_const),
+        )
+    }};
+}
+
+#[macro_export]
 macro_rules! v {
     ($osc:expr, $lower_const:expr, $upper_const:expr) => {{
         let prev_osc = $osc.shift(1);
