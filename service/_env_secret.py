2786be73733b27260af8c9ddda352969384d756c
m5l14i11 <siarhei.melnik@proton.me>
2024-08-19 22:46:12 +0300

    upd

--- service/_env_secret.py
+++ service/_env_secret.py
@@ -1,17 +1,36 @@
+import logging
 import os
+from typing import Optional
 
 from core.interfaces.abstract_secret_service import AbstractSecretService
+
+logger = logging.getLogger(__name__)
 
 
 class EnvironmentSecretService(AbstractSecretService):
     def __init__(self):
         super().__init__()
 
-    def get_api_key(self, identifier: str) -> str:
-        return os.environ.get(identifier + "_API_KEY")
+    def get_api_key(self, identifier: str) -> Optional[str]:
+        key = f"{identifier.upper()}_API_KEY"
 
-    def get_secret(self, identifier: str) -> str:
-        return os.environ.get(identifier + "_API_SECRET")
+        return self._get_env_variable(key)
 
-    def get_wss(self, identifier: str) -> str:
-        return os.environ.get(identifier + "_WSS")
+    def get_secret(self, identifier: str) -> Optional[str]:
+        key = f"{identifier.upper()}_API_SECRET"
+
+        return self._get_env_variable(key)
+
+    def get_wss(self, identifier: str) -> Optional[str]:
+        key = f"{identifier.upper()}_WSS"
+
+        return self._get_env_variable(key)
+
+    @staticmethod
+    def _get_env_variable(key: str) -> Optional[str]:
+        value = os.environ.get(key)
+
+        if value is None:
+            logger.warning(f"Environment variable '{key}' is not set.")
+
+        return value
