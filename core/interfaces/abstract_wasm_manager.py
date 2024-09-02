cf5ec7a91c12151308ba5741eb4b6267bf60f47d
m5l14i11 <siarhei.melnik@proton.me>
2024-09-02 17:38:14 +0300

    upd

--- core/interfaces/abstract_wasm_manager.py
+++ core/interfaces/abstract_wasm_manager.py
@@ -8,9 +8,5 @@ from core.models.wasm_type import WasmType
 
 class AbstractWasmManager(ABC):
     @abstractmethod
-    def load_instance(self, wasm_type: WasmType):
-        pass
-
-    @abstractmethod
     def get_instance(self, wasm_type: WasmType) -> Tuple[Instance, Store]:
         pass
