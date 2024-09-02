904486afd7a634424bbb837626210a0e7f07a29e
m5l14i11 <siarhei.melnik@proton.me>
2024-09-02 13:51:48 +0300

    upd

--- service/_wasm.py
+++ service/_wasm.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 from wasmtime import Engine, Linker, Module, Store, WasiConfig
 
@@ -15,27 +16,35 @@ class WasmManager(AbstractWasmManager):
     def __init__(self, dir="wasm"):
         super().__init__()
         self.dir = dir
-        self.instances = {}
 
-    def load_instance(self, wasm_type: WasmType):
+    @lru_cache(maxsize=None)
+    def get_instance(self, wasm_type: WasmType):
+        return self._load_instance(wasm_type)
+
+    def _load_instance(self, wasm_type: WasmType):
         store = Store()
-        self._configure_wasi(store)
-        linker = Linker(store.engine)
-        linker.define_wasi()
+
+        wasi = self._configure_wasi()
+
+        store.set_wasi(wasi)
+
+        linker = self._configure_linker(store.engine)
+
         module = self._get_module(wasm_type, store.engine)
         instance = linker.instantiate(store, module)
-        self.instances[wasm_type] = (instance, store)
 
-    def get_instance(self, wasm_type: WasmType):
-        if wasm_type not in self.instances:
-            self.load_instance(wasm_type)
-        return self.instances[wasm_type]
+        return (instance, store)
 
-    def _configure_wasi(self, store: Store):
+    def _configure_wasi(self):
         wasi_config = WasiConfig()
         wasi_config.wasm_multi_value = True
         wasi_config.inherit_stdout()
-        store.set_wasi(wasi_config)
+        return wasi_config
+
+    def _configure_linker(self, engine: Engine) -> Linker:
+        linker = Linker(engine)
+        linker.define_wasi()
+        return linker
 
     def _get_module(self, type: WasmType, engine: Engine) -> Module:
         if type not in WasmType:
