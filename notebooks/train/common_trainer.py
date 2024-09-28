6c38b3587e3b30c5782bc089a2d8ffa9e60ac977
m5l14i11 <siarhei.melnik@proton.me>
2024-09-28 13:26:36 +0300

    upd

--- notebooks/train/common_trainer.py
+++ notebooks/train/common_trainer.py
@@ -1,22 +1,43 @@
+import gc
+
+import torch
+
 from .trainer import Trainer
 
 
 class CommonTrainer(Trainer):
+    def __init__(self, *args, acc_steps=4, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.acc_steps = acc_steps
+
     def train_epoch(self):
         running_loss = 0.0
+        self.optimizer.zero_grad()
 
-        for _batch_idx, (data,) in enumerate(self.dataloader):
+        for batch_idx, (data,) in enumerate(self.dataloader):
             data = data.to(self.device)
 
-            self.optimizer.zero_grad()
-
             outputs = self.model(data)
-            loss = self.criterion(outputs, data)
 
+            loss = self.criterion(outputs, data)
+            loss = loss / self.acc_steps
             loss.backward()
-            self.optimizer.step()
+
+            if (batch_idx + 1) % self.acc_steps == 0:
+                self.optimizer.step()
+                self.optimizer.zero_grad()
 
             running_loss += loss.item()
+
+            del [data, outputs, loss]
+
+            if torch.backends.mps.is_available():
+                torch.mps.empty_cache()
+
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+
+            gc.collect()
 
         avg_train_loss = running_loss / len(self.dataloader)
 
